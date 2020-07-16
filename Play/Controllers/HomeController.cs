0c248d5c7aff976fe655ec2089db25475e4b1ec2
Upendra Gusain <upendra.gusain@gmail.com>
2020-07-16 15:23:18 +0100

    use addmvc for routing to home/index

---
+++ Play/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Play.Models;
+
+namespace Play.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var employees = new List<Employee>();
+            employees.Add(new Employee(1));
+            employees.Add(new Employee(2));
+            employees.Add(new Employee(3));
+
+            return View(employees);
+        }
+    }
+}
--- Play/Startup.cs
+++ Play/Startup.cs
@@ -16,6 +16,7 @@ namespace Play
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +31,9 @@ namespace Play
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default", 
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
